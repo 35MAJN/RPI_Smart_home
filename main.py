838df536aee4d79c3afeb53126a137b22c9301f6
MAJN35 <majnnjam@gmail.com>
2021-07-22 05:14:36 +0430

    1350% of misson Completed With backs

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from telegram import Bot
 from Syntax_UI import SyntaxUI
 
 if __name__ == "__main__":
-    bot = Bot(token='token', base_url="https://tapi.bale.ai/")
+    bot = Bot(token='783741740:c34f5dad998ac3dc63010957cb5f929e7d0d6d3e', base_url="https://tapi.bale.ai/")
 
     app = QApplication(argv)
     form = SyntaxUI(bot)
